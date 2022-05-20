3a8d3b440b534e1fb6e233f2f5e74a41696de56c
vasil <vaskonikolov2003@gmail.com>
2022-05-20 21:58:49 +0300

    cyclic buffer framework works!

--- src/main.rs
+++ src/main.rs
@@ -3,68 +3,39 @@
 use std::thread;
 use std::time::Duration;
 
-extern crate num;
-use num::complex::Complex;
 
 mod user_input;
 mod term_io;
 mod cyclic_buffer;
+mod mandelbrot;
 
-fn render_mandlebrot(screen: &mut term_io::Screen) -> Result<(), &'static str> {
-    let (w, h) = screen.term_size;
-    let N = 100;
-    let mut z_norm;
-    for x in 0..w {
-        for y in 0..h {
-            let mut in_set = true;
-            let mut z = Complex::new(0.0, 0.0); 
-            let c = screen.get_complex_coords(x, y)?;
-            for i in 0..N {
-                z = z*z + c; 
-                z_norm = z.norm_sqr(); 
-                if z_norm > 4.0 {
-                    in_set = false;
-                    break;
-                }
-                else if z_norm < 0.01 {
-                    break;
-                }
-            }
-            if in_set {
-                screen.putchar(x, y, '#')?;
-            }
-        }
-    } 
-    screen.flush_screen()?;
-    Ok(())
-}
 fn main() {
     if let Err(e) = run() {
         println!("main exited with error: {}", e);
     }
 }
 
-fn test_run() -> Result<(), &'static str> {
-    let mut screen = term_io::setup_terminal();
-    screen.clear_screen()?;
-    let mut buffer: cyclic_buffer::Buffer<Option<char>> = cyclic_buffer::Buffer::new(screen.term_size, None);
-    buffer.put(Some('a'), 0, 0)?;
-    buffer.put(Some('b'), 1, 1)?;
-    buffer.put(Some('j'), 2, 2)?;
-    buffer.put(Some('d'), 3, 3)?;
-    buffer.shift(cyclic_buffer::Direction::Down, 3, None)?;
-    buffer.shift(cyclic_buffer::Direction::Right, 3, None)?;
-    for y in 0..buffer.size.1 {
-        for x in 0..buffer.size.0 {
-            if let Some(c) = buffer.get(x, y)? {
-                screen.putchar(x, y, c)?;
-            }
-        }
-    } 
-    screen.flush_screen()?;
-    thread::sleep(Duration::from_millis(2000));
-    Ok(())
-}
+// fn test_run() -> Result<(), &'static str> {
+//     let mut screen = term_io::setup_terminal();
+//     screen.clear_screen()?;
+//     let mut buffer: cyclic_buffer::Buffer<Option<char>> = cyclic_buffer::Buffer::new(screen.term_size, None);
+//     buffer.put(Some('a'), 0, 0)?;
+//     buffer.put(Some('b'), 1, 1)?;
+//     buffer.put(Some('j'), 2, 2)?;
+//     buffer.put(Some('d'), 3, 3)?;
+//     buffer.shift(cyclic_buffer::Direction::Down, 3, None)?;
+//     buffer.shift(cyclic_buffer::Direction::Right, 3, None)?;
+//     for y in 0..buffer.size.1 {
+//         for x in 0..buffer.size.0 {
+//             if let Some(c) = buffer.get(x, y)? {
+//                 screen.putchar(x, y, c)?;
+//             }
+//         }
+//     } 
+//     screen.flush_screen()?;
+//     thread::sleep(Duration::from_millis(2000));
+//     Ok(())
+// }
 fn run() -> Result<(), &'static str>{
     let mut screen = term_io::setup_terminal();
     
@@ -75,17 +46,18 @@ fn run() -> Result<(), &'static str>{
     let move_speed = 0.02;
     loop {
         screen.clear_screen()?;
-        render_mandlebrot(&mut screen)?;
+        mandelbrot::render_whole_mandelbrot(&mut screen)?;
+        screen.render()?;
         loop {
             let c = user_input::get_char(&mut screen);
             match c {
                 None => continue,
                 Some('q') => {should_end_program = true; break;}, 
                 // movement controlls
-                Some('h') => {screen.center += Complex::new(move_speed, 0.0); break;}
-                Some('l') => {screen.center += Complex::new(-move_speed, 0.0); break;}
-                Some('j') => {screen.center += Complex::new(0.0, move_speed); break;}
-                Some('k') => {screen.center += Complex::new(0.0, -move_speed); break;}
+                // Some('h') => {screen.center += Complex::new(move_speed, 0.0); break;}
+                // Some('l') => {screen.center += Complex::new(-move_speed, 0.0); break;}
+                // Some('j') => {screen.center += Complex::new(0.0, move_speed); break;}
+                // Some('k') => {screen.center += Complex::new(0.0, -move_speed); break;}
                 // zoom control
                 Some('z') => {screen.scale *= zoom_speed; break;}
                 Some('x') => {screen.scale /= zoom_speed; break;}
--- src/term_io.rs
+++ src/term_io.rs
@@ -4,6 +4,7 @@ use num::complex::Complex;
 use termion::raw::IntoRawMode;
 use termion::async_stdin;
 use std::io::{Write, stdout};
+use crate::cyclic_buffer::Buffer;
 
 pub struct Screen {
     pub stdin: termion::AsyncReader, 
@@ -11,6 +12,7 @@ pub struct Screen {
     pub term_size: (u16, u16), 
     pub scale: f64, 
     pub center: Complex<f64>,
+    pub buffer: Buffer<Option<char>>,
 }
 
 pub fn setup_terminal() -> Screen {
@@ -18,6 +20,7 @@ pub fn setup_terminal() -> Screen {
     let stdout = stdout.into_raw_mode().unwrap();
     let stdin = async_stdin();
     let (w, h) = termion::terminal_size().unwrap();
+    let buffer: Buffer<Option<char>> = Buffer::new((w, h), None);
 
     Screen {
         stdin: stdin, 
@@ -25,6 +28,7 @@ pub fn setup_terminal() -> Screen {
         term_size: (w, h),
         scale: 0.02, 
         center: Complex::new(0.0, 0.0),
+        buffer: buffer 
     }
 }
 
@@ -48,23 +52,32 @@ impl Screen {
         }
         Ok(())
     }
-
-    pub fn putchar(&mut self, x: u16, y: u16, c: char) -> Result<(), &'static str>{
-        let res = write!(self.stdout,
-               "{}{}",
-               termion::cursor::Goto(x + 1, y + 1), 
-               c);
-
-        if let Err(_e) = res {
-            return Err("could not clear screen");
-        }
+    pub fn putchar(&mut self, x: u16, y: u16, c: Option<char>) -> Result<(), &'static str>{
+        self.buffer.put(c, x, y)?;
         Ok(())
     }
-
     pub fn flush_screen(&mut self) -> Result<(), &'static str> {
         if let Err(e) = self.stdout.flush() {
             return Err("could not flush screen");
         }
+        Ok(())
+    }
+    pub fn render(&mut self) -> Result<(), &'static str> {
+        for x in 0..self.term_size.0 {
+            for y in 0..self.term_size.1 {
+                let c = self.buffer.get(x, y)?;
+                if let None = c {
+                    return Err("cannot render screen with None pixels");
+                }
+                let res = write!(self.stdout,
+                       "{}{}",
+                       termion::cursor::Goto(x + 1, y + 1), 
+                       c.unwrap());
+                if let Err(_e) = res {
+                    return Err("could not write to screen during render");
+                }
+            }
+        } 
         Ok(())
     }
 }
